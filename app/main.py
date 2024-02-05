e82b3e6dd4c3423f6b69be6f35ff6d7ca6a7d3ef
Oliver Bell <141230634+Oliverbell97@users.noreply.github.com>
2024-02-05 20:05:16 +0000

    update

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from pydantic import BaseModel
 from app.database import SessionLocal, engine
 from sqlalchemy.orm import Session
 import app.models as models
-from sqlalchemy import select, delete
+from sqlalchemy import select, delete, update
 
 app = FastAPI()
 
@@ -38,14 +38,25 @@ def read_root(db: db_dependency):
 
 @app.post("/friend")
 def add_friend(friend: NewFriend, db: db_dependency):
-    db.add(friend)
+    db_friend = models.Friend(name=friend.name, location=friend.location, friends=friend.friends)
+    db.add(db_friend)
     db.commit()
-    return friend
+    return 'Friend Added'
 
-@app.post('/FRIENDS/{friend_id}')
+@app.delete('/FRIENDS/{friend_id}')
 def remove_friend(friend_id: int, db: db_dependency):
     db_query = select(models.Friend).where(models.Friend.id == friend_id)
     for x in db.scalars(db_query):
         db.delete(x)
     db.commit()
+    return "Friend Removed"
 
+@app.put('/FRIENDS/{update}')
+def update_friend(friend: NewFriend, friend_id: int, db: db_dependency):
+    new_name = friend.name
+    new_location = friend.location
+    new_friends = friend.friends
+    update(models.Friend).where(models.Friend.id == friend_id).values(models.Friend.name == new_name, models.Friend.location == new_location, models.Friend.friends == new_friends)
+    
+
+    return
